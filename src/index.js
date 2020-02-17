891a1a91272fa356c189ed2b2bba8eadfabbac2a
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-02-16 21:36:54 -0500

    done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length !== b.length) {
+    throw new Error('Por lo menos botelas igual!! >:(');
   }
+
+  let distance = 0;
+
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      distance += 1;
+    }
+  }
+
+  return distance;
+}
   
   module.exports = hammingDistance;
