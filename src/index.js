72c3fe753485708f212905f628e70bbe7d89846a
dfloresdev <hello@dflores.dev>
2020-02-23 23:42:32 -0600

    Mission passed
    respect+

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let cont = 0;
+  if (a.length === b.length) {
+    for (let i = 0; i < a.length; i++) {
+      if (a.charAt(i) !== b.charAt(i)) {
+        cont++;
+      }
+    }
+    return cont;
+  } else {
+    return 0;
   }
-  
-  module.exports = hammingDistance;
+};
+
+module.exports = hammingDistance;
