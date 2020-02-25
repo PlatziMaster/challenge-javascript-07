30a3f1b9f01e3b46e8b5ef14e73d2fda2f14f237
MauricioFa <mauriciositel@hotmail.com>
2020-02-25 09:36:56 -0500

    Hamming Distance challenge solved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  var number = 0
+  if (a.length === b.length) {
+    for (i = 0; i < a.length; i++) {
+      if (a.charAt(i) !== b.charAt(i))
+        number++
+    }
+    return number
+  } else {
+    throw new Error('strings given don´t have the same length')
   }
-  
-  module.exports = hammingDistance;
+}
+
+module.exports = hammingDistance;
