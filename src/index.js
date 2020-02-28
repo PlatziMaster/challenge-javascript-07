fe149f72e5ef14ef6f8ede097243732a1ba62b97
luis_parra <luis.parra.pulido@gmail.com>
2020-02-28 18:57:55 -0500

    reto terminado

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let distance = 0;
+    if ( a.length !== b.length) {
+      throw 'You cannot compare strings of different lengths'
+      
+    } 
+
+    for(i=0;i<a.length;i++) {
+      if(a.charAt(i) !== b.charAt(i)) {
+        distance = distance + 1;
+      }
+    }
+    return distance
   }
+
+hammingDistance('a', 'a')
   
   module.exports = hammingDistance;
