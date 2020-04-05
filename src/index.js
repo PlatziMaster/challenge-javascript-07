c8315859a581a611bab5c32f6f0c57a470ba6268
Jared Ortega <iamyoujared@MacBook-Air-de-Jared.local>
2020-04-05 00:36:59 -0600

    Reto resuelto

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@
  * @return {number}
  */
 
-const hammingDistance = (a, b) => {
-    
-  }
+const hammingDistance = (str1, str2) => {
+    let distance = 0;
+    if ( str1.length !== str2.length) {
+      throw 'You cannot compare strings of different lengths';
+    } 
+
+    for(i = 0; i < str1.length; i++) {
+      if(str1[i] !== str2[i]) distance += 1;
+    }
+    return distance
+}
   
   module.exports = hammingDistance;
