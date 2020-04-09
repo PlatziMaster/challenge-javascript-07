4939d2a3708f9d5c35e9a654b717335a404d5101
J. Alvarez <jcalvarezj@gmail.com>
2020-04-09 10:14:28 -0500

    Solved Challenge

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@
  * @param {string} b
  * @return {number}
  */
-
 const hammingDistance = (a, b) => {
-    
+  if (a.length !== b.length)
+    throw new Error('The lengths of a and b should be equal!');
+  else {
+    let count = 0;
+    for (let i = 0; i < a.length; i++)
+      if (a[i] !== b[i])
+        count++;
+    return count;
   }
+}
   
-  module.exports = hammingDistance;
+module.exports = hammingDistance;
