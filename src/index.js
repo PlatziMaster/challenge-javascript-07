520d9cb6a91eb424b7c9822ef4a6d6c016694051
J. Alvarez <jcalvarezj@gmail.com>
2020-05-05 16:29:33 -0500

    Improved code according to PR comments

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  */
 const hammingDistance = (a, b) => {
   if (a.length !== b.length)
-    throw new Error('The lengths of a and b should be equal!');
+    throw new Error(`The lengths of ${a} and ${b} should be equal!`);
   else {
     let count = 0;
     for (let i = 0; i < a.length; i++)
