eaa1fca1e2abe90509bd74b7908ec10f38d73e9c
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-05-08 14:43:05 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let count = 0;
+  if (a.length === b.length) {
+    for (let i = 0; i <= a.length; i++) {
+      if (a[i] !== b[i]) count += 1;
+    }
+  } else {
+    throw 'String should have same length!';
   }
-  
-  module.exports = hammingDistance;
+
+  return count;
+};
+
+module.exports = hammingDistance;
