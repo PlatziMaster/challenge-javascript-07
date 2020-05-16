17308bd446b87ec1a89f6d6435c7500345839b36
hadiazb <hadiazb@unal.edu.co>
2020-05-15 19:37:07 -0500

    Reto terminado

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@
  */
 
 const hammingDistance = (a, b) => {
+    let z = 0;
+    if (a.length == b.length) {
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) {
+          z++;
+        }
+      }
+      return z;
+    } else {
+      throw new Error( "Something bad happened." );;
+    }
     
   }
+  hammingDistance('a', 'b');
   
   module.exports = hammingDistance;
