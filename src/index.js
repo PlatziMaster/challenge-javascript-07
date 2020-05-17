511ab9b9fe7ffee8353f8fd3c06d3f488c30d6e1
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-16 20:38:43 -0500

    implementando reto distancia Hamming

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length == b.length) {
+    let distance = 0;
+    for (let index = 0; index < a.length; index++) {
+      // The charAt() method returns the character at the specified index in a string.
+      if (a.charAt(index) != b.charAt(index)) distance++;
+    }
+    return distance;
+  } else {
+    throw new Error("Las palabras o cadenas deben ser de la misma longitud.");
   }
-  
-  module.exports = hammingDistance;
+};
+
+module.exports = hammingDistance;
