8e93c1447f4bf8cf73579b11e29e73e53a7021f3
Miguel Angel Martelo Quiroz <miguelmartelo@seven4n.com>
2020-05-21 22:14:12 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let number = 0;
+  if (a.length !== b.length) {
+    throw new Error("Las cadenas deben tener la misma longitud");
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      a[i] !== b[i] ? number++ : number;
+    }
   }
-  
-  module.exports = hammingDistance;
+
+  return number;
+};
+
+module.exports = hammingDistance;
