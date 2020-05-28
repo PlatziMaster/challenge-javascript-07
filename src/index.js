31d167117746a702122707de496eab3e5d06adde
emanuelosva <emanuelosva@gmail.com>
2020-05-28 08:38:32 -0500

    Solution

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length !== b.length) {
+    throw Error;
   }
-  
-  module.exports = hammingDistance;
+
+  const serieA = a.split('');
+  const serieB = b.split('');
+  let count = 0;
+
+  for (let i = 0; i <= a.length; i++) {
+    serieA[i] !== serieB[i] ? count++ : null;
+  }
+  return count;
+}
+
+module.exports = hammingDistance;
