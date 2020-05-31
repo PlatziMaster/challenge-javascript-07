a52c33fbcb5100cf063abaac57998180a8e6919a
Jedp <jedp082@gmail.com>
2020-05-31 14:18:59 -0500

    Challenge done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+
+  if(a.length != b.length){
+    throw new Error;
   }
+  let distance = 0;
+
+  for(let i = 0; i < a.length; i++){
+    if(a[i] !== b[i]){
+      distance += 1;
+    }
+  }
+
+  return distance;
+}
+
+hammingDistance('a', 'a')
+hammingDistance('a', 'b')
+hammingDistance('abc', 'add')
+hammingDistance('karolin', 'kathrin')
+hammingDistance('karolin', 'kerstin')
+hammingDistance('1011101', '1001001')
+hammingDistance('2173896', '2233796')
   
   module.exports = hammingDistance;
