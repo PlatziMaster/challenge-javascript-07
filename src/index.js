72be0794193bc0fcc602a155eb63a09627dc397a
Jhon L Guerrero <jhoguer@gmail.com>
2020-05-30 22:04:22 -0500

    Resolviendo reto de distancia de hamming

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@
  * @return {number}
  */
 
+
+
 const hammingDistance = (a, b) => {
+  if(a.length !== b.length) 
+    throw new Error('the length of the string is different');
+
+  let count = 0;
+  for(let i = 0; i < a.length; i++) {
+    if((a.charAt(i) !== b.charAt(i))) {
+      count++;
+    }
     
   }
-  
-  module.exports = hammingDistance;
+  return count
+}
+
+
+module.exports = hammingDistance;
