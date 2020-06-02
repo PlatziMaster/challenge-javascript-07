3ddba1771bc1fcebb222e8b21441bec1a8db47c1
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-02 18:38:32 -0500

    Hamming distance solved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length !== b.length) {
+    throw new Error('Strings must have equal length');
   }
-  
-  module.exports = hammingDistance;
+
+  let distance = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      distance++;
+    }
+  }
+  return distance;
+};
+
+module.exports = hammingDistance;
