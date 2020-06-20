5a8a4be77a0cf98b46920fd8eb0cd3334b0aeab9
Osvaldo Arzate <osvarz.new@gmail.com>
2020-06-20 14:32:40 -0500

    termina reto

--- src/index.js
+++ src/index.js
@@ -5,7 +5,31 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  const result = [];
+  if (a.length === b.length) {
+    let textA = " ";
+    let textB = " ";
+    for (let i = 0; i < a.length; i++) {
+      textA += a[i].charCodeAt(0).toString(2) + " ";
+    }
+
+    for (let i = 0; i < b.length; i++) {
+      textB += b[i].charCodeAt(0).toString(2) + " ";
+    }
+
+    for (let i = 0; i < a.length; i++) {
+      result.push(a.charAt(i) === b.charAt(i))
+    }
+    const diference = result.filter((item) => {
+      return item === false;
+    })
+
+    return diference.length;
+  } else {
+    throw "error"
   }
-  
-  module.exports = hammingDistance;
+}
+
+hammingDistance('10', '10');
+
+module.exports = hammingDistance;
