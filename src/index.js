e8d77ed6abbd0069073c1690adbeae9dbf0b53b3
Jesus Mejia <chucho4720013@gmail.com>
2020-07-02 01:20:10 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length === b.length) {
+    let distance = 0
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) distance++
+    }
+    return distance
   }
+  
+  throw new Error ('Strings of different lenght')
+}
   
   module.exports = hammingDistance;
