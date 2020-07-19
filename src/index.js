7311a8fad5004bfadbef910fa569af2c9ff5505d
ilder948 <ilder948@gmail.com>
2020-07-19 16:13:36 -0500

    Reto Cumplido

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let diferencia = 0
+
+
+  if (a.length !== b.length){
+    throw new Error('Parametros deben tener la misma longitud')
   }
-  
+    for (let i = 0; i < a.length; i++) {
+      if (a.charAt(i) !== b.charAt(i)) {
+        diferencia++
+      }
+    }
+  return diferencia
+  }  
+
   module.exports = hammingDistance;
