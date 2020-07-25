5bd75b5a31502c402bc5a0ecadcef9b09e0c23f1
Gerardo Garavito <garavitogerardo@gmail.com>
2020-07-25 16:11:22 -0500

    challenge05 done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let cont = 0;
+
+  if (a.length != b.length) {
+    throw console.error('error');
   }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a.charAt(i) != b.charAt(i)){
+      cont++
+    }
+  }
+
+  return cont
+}
   
   module.exports = hammingDistance;
