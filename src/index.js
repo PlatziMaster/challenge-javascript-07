dbc09d0228265ad37abbd53328626d5f9c59a637
Daniel Valderrama <dny9919@gmail.com>
2020-07-30 21:34:03 -0500

    challenge ready

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  const long1 = a.length
+  const long2 = b.length
+  var number = 1
+  function filter1(valor1, valor2) {
+    if (valor1 === valor2) {
+      function lecturaCharacter(clave, clave2) {
+        for (var i = 0; i < clave.length; i++) {
+          if ((clave.charAt(i) === clave2.charAt(i)) === false) {
+            number = (number + 1)
+            return number
+          } 
+        }
+      }
+      lecturaCharacter(a, b)
+    } else {
+      throw new Error("Whoops!");
+    }
   }
-  
-  module.exports = hammingDistance;
+  filter1(long1, long2)
+  return number
+}
+
+hammingDistance('holas', 'halos')
+module.exports = hammingDistance;
