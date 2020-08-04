56a2c6fc6e2334663f9530466e96ae7a51c7c72f
Sergio Ruiz <shecho27@gmail.com>
2020-08-03 21:09:08 -0500

    Edit the function: Hamming Distance

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  if (a.length === b.length) {
+    let distancia = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (a.charAt(i) != b.charAt(i)) {
+        distancia++;
+      }
+    }
+    return distancia;
+  } else {
+    throw new Error(`Los datos no coinciden`);
   }
-  
-  module.exports = hammingDistance;
+};
+
+module.exports = hammingDistance;
