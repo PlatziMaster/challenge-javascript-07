46454b80200233c59e0f43e2f1d614568b5c35c3
Jaime Escobedo <maildeljimmy@gmail.com>
2020-08-16 21:51:58 -0500

    Reto Terminado Jaime Escobedo

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@
  * @return {number}
  */
 
-const hammingDistance = (a, b) => {
-    
+const hammingDistance = (a, b) => {debugger
+  var arrayA = a.split(""), arrayB = b.split(""), numberDistance = 0
+  if (!(arrayA.length == arrayB.length)) {
+      throw error 
   }
-  
+  //('Si tienen misma longitud')
+  for (var i = 0; i <= arrayA.length-1; i++){
+      if (!(arrayA[i] === arrayB[i]))
+      numberDistance ++
+  }return numberDistance
+}
   module.exports = hammingDistance;
