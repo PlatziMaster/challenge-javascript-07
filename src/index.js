c5524078ae70300c90ad068065140d05d4a62add
carlosvldz <me@carlosvldz.com>
2020-08-26 20:16:53 -0500

    carlosvldzzz - challenge completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+    let result = 0
+    if(a.length === b.length) {
+      for(let i = 0; i < a.length; i++) {
+        if(a[i].toLowerCase() != b[i].toLowerCase()) {
+          result++
+        }
+      }
+      return result
+    }else {
+      throw new Error('las cadenas tienen diferente longitud')
+    }
   }
   
   module.exports = hammingDistance;
