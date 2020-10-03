00f56478b29a81a1015d486e52cf84c34d52e841
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-03 18:44:58 -0500

    Creating function for hammer distance

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@
  * @return {number}
  */
 
-const hammingDistance = (a, b) => {
-    
+function distance(a) {
+  const lengthA = a.length
+  return function hamming(b) {
+    let total = 0
+    for (let i = 0; i < lengthA; i++) {
+      if (a[i] !== b[i]) {
+        total++
+      }
+    }
+    return total
   }
-  
-  module.exports = hammingDistance;
+}
+
+const hammingDistance = (a, b) => {
+  if (a.length !== b.length) {
+    throw new Error('Strings should be same lengths')
+  }
+  const distanceHamming = distance(a)
+  return distanceHamming(b)
+}
+
+module.exports = hammingDistance;
